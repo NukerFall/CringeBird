843f4a943ee89f300452d13f9389230cab3c230c
NukerFall <47152422+NukerFall@users.noreply.github.com>
2022-03-02 01:20:16 +0300

    Update cringe.cpp

--- cringe.cpp
+++ cringe.cpp
@@ -232,3 +232,4 @@ int main() {
 	return 0;
 } // https://drive.google.com/drive/folders/1uLR7bCBXwraaLCXnnXn3_iuQL3AqLxkB?usp=sharing
 // https://docs.google.com/document/d/1vtbMHPPkKMDfc0AykarOs_3Kvk1ku2dK/edit?usp=sharing&ouid=108777646462001550507&rtpof=true&sd=true
+// https://docs.google.com/document/d/1PYNhX2t4IA-J5ro29eiEnireeZgK-gml/edit?usp=sharing&ouid=108777646462001550507&rtpof=true&sd=true
